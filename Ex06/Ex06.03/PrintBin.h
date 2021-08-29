9436c07ea27de75e94c8034a745e7410f704d765
Avraham Meyers <a32100meyers@gmail.com>
2021-08-29 23:15:16 +0300

    added descriptions

--- Ex06/Ex06.03/PrintBin.h
+++ Ex06/Ex06.03/PrintBin.h
@@ -1,16 +1,17 @@
 #pragma once
 
-/// @brief 
+/// @brief using a macro for base 16
 #define HexBase 16
 
-
+/// @brief a pointer to a single byte
 typedef unsigned char* BytePointer;
 
-/// @brief 
-/// @param address 
-/// @param num_bytes 
+/// @brief prints the sequence of bytes pointed to address in binary
+/// @param address the address of the sequence of bytes
+/// @param num_bytes the number of bytes in the sequence
 void PrintBin(BytePointer address, size_t num_bytes);
 
+/// @brief a struct for storing the two digits in hexadecimal for a byte
 typedef struct HexByte_t
 {
     short tens;
@@ -18,6 +19,11 @@ typedef struct HexByte_t
 
 } HexByte;
 
+/// @brief receives a decimal number and returns a HexByte with the corresponding hexadecimal digits
+/// @param num the number to convert to hexadecimal
+/// @return the HexByte with the corresponding hexadecimal values
 HexByte *DecToHex(char num);
 
+/// @brief takes a number between 1-15 and prints its binary form
+/// @param number the number to print in binary
 void PrintHexinBin(short number);
--- Ex06/Ex06/List.h
+++ Ex06/Ex06/List.h
@@ -1,7 +1,9 @@
 #pragma once
 
+/// @brief the type of the data of the list
 typedef int ListDataType;
 
+/// @brief a struct for the nodes of the list, with a previous and next pointer
 typedef struct Node_t 
 {
     ListDataType key;
@@ -9,36 +11,83 @@ typedef struct Node_t
     struct Node_t* prev;
 } Node;
 
+/// @brief a struct for the list with a head node
 typedef struct List_t 
 {
     Node* head;
 } List;
 
+/// @brief creates a list
+/// @param no parameters 
+/// @return a pointer to the list created
 List* CreateList(void);
 
+/// @brief deletes all of the memory used for the list
+/// @param list the list to delete
 void FreeList(List* list);
 
+/// @brief returns the length of the list
+/// @param list the list to check
+/// @return the length of the list
 size_t GetListSize(List* list);
 
+/// @brief returns a pointer to the head of the list
+/// @param list the list to check for the head
+/// @return a pointer to the head of the list
 Node* GetListHead(List* list);
 
+/// @brief returns the next node after the node passed
+/// @param currentNode the node to check for the next node
+/// @return a pointer to the next node after the current node
 Node* GetNextNode(Node* currentNode);
 
+/// @brief returns the value of the node passed
+/// @param node the node to check for a value
+/// @return the value of the key of the node
 ListDataType GetValue(Node* node);
 
+/// @brief inserts a node with the passed value after the passed node
+/// @param after the node to place before the new node
+/// @param newValue the value of the key of the new node
+/// @return a pointer to the new node
 Node* Insert(Node* after, ListDataType newValue);
 
+/// @brief adds a new node with the passed value to the head of the list
+/// @param list the list to add the value to
+/// @param newValue the value to add to the list 
+/// @return a pointer to the new head of the list
 Node* PushFront(List* list, ListDataType newValue);
 
+/// @brief adds a new node to the end of the list with the passed value
+/// @param list the list to add the value to 
+/// @param newValue the value of the new node to add
+/// @return a pointer to the new node
 Node* PushBack(List* list, ListDataType newValue);
 
+/// @brief removes the head of the list and returns the value in the key of the node
+/// @param list the list to remove the head from
+/// @return the value of the head of the list
 ListDataType PopFront(List* list);
 
+/// @brief removes the last node from the list and returns the value from the node
+/// @param list the list to remove the last node from
+/// @return the value of the last node of the list
 ListDataType PopBack(List* list);
 
+/// @brief deletes the target node from the list
+/// @param list the list to remove the node from
+/// @param target the node to remove from the list
 void DeleteNode(List* list, Node* target);
 
+/// @brief deletes the node from the list with the passed value as its key
+/// @param list the list to remove the value from
+/// @param value the value to remove from the list
+/// @return a oointer to the deleted node
 Node* DeleteValue(List* list, ListDataType value);
 
+/// @brief returns a pointer to the node in the list with the passed value
+/// @param list the list to check for the value
+/// @param value the value to look for
+/// @return a pointer to the node in the list with the passed value
 Node* Find(List* list, ListDataType value);
 
